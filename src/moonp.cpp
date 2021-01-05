900e4d6548c15461653583c4e70c3658d0b60d17
Li Jin <dragon-fly@qq.com>
2021-01-05 10:07:11 +0800

    update year.

--- src/moonp.cpp
+++ src/moonp.cpp
@@ -1,4 +1,4 @@
-/* Copyright (c) 2020 Jin Li, http://www.luvfight.me
+/* Copyright (c) 2021 Jin Li, http://www.luvfight.me
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
@@ -85,10 +85,10 @@ static const char luaminifyCodes[] =
 
 static void pushLuaminify(lua_State* L) {
 	if (luaL_loadbuffer(L, luaminifyCodes, sizeof(luaminifyCodes) / sizeof(luaminifyCodes[0]) - 1, "=(luaminify)") != 0) {
-		std::string err = std::string("fail to load luaminify module.\n") + lua_tostring(L, -1);
+		std::string err = std::string("failed to load luaminify module.\n") + lua_tostring(L, -1);
 		luaL_error(L, err.c_str());
 	} else if (lua_pcall(L, 0, 1, 0) != 0) {
-		std::string err = std::string("fail to init luaminify module.\n") + lua_tostring(L, -1);
+		std::string err = std::string("failed to init luaminify module.\n") + lua_tostring(L, -1);
 		luaL_error(L, err.c_str());
 	}
 }
