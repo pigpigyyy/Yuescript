f69efffdebe60833e6e5da89850347930aa53a22
Li Jin <dragon-fly@qq.com>
2023-12-11 09:42:10 +0800

    update version.

--- src/yuescript/yue_compiler.cpp
+++ src/yuescript/yue_compiler.cpp
@@ -75,7 +75,7 @@ static std::unordered_set<std::string> Metamethods = {
 	"close"s // Lua 5.4
 };
 
-const std::string_view version = "0.21.0"sv;
+const std::string_view version = "0.21.1"sv;
 const std::string_view extension = "yue"sv;
 
 class CompileError : public std::logic_error {
