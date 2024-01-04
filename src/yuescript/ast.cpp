7801ea7755f6688dd0665ccb38f369dab80ca2c7
Li Jin <dragon-fly@qq.com>
2024-01-04 10:45:00 +0800

    happy new year!

--- src/yuescript/ast.cpp
+++ src/yuescript/ast.cpp
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012, Achilleas Margaritis, modified by Jin Li
+/* Copyright (c) 2012, Achilleas Margaritis, modified by Li Jin
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
